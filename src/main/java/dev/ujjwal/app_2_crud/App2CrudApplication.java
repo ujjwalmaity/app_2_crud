12aa4b5236829e44a2eb00ebcb28b27ae9fceb0e
Ujjwal Maity <ujjwalmaity98@gmail.com>
2024-11-19 19:43:19 +0530

    Initial commit

---
+++ src/main/java/dev/ujjwal/app_2_crud/App2CrudApplication.java
@@ -0,0 +1,13 @@
+package dev.ujjwal.app_2_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App2CrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App2CrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/ujjwal/app_2_crud/App2CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ujjwal.app_2_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class App2CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
